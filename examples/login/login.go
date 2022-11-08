29eafed7988f1f0b324ab72b5d0fb4fb756aeeb1
drummonds <hum3@drummond.info>
2022-11-08 21:59:34 +0000

    Have reformatted got vet to pass but not yet docker 1

---
+++ examples/login/login.go
@@ -0,0 +1,30 @@
+// Copyright © 2021 Kris Nóva <kris@nivenly.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package main
+
+import (
+	photoprism "github.com/drummonds/photoprism-client-go"
+	common "github.com/drummonds/photoprism-client-go/examples/common"
+	"github.com/kris-nova/logger"
+)
+
+func main() {
+	logger.Level = 4
+	client := photoprism.New("http://localhost:8080")
+	err := client.Auth(photoprism.NewClientAuthLogin("admin", "missy"))
+	if err != nil {
+		common.Halt(4, "Error logging into API: %v", err)
+	}
+	logger.Always("Login Success!")
+}
